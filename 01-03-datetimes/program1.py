3465ea90fb62cc679821d0c3265668d179ad9376
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2018-12-07 23:37:47 +0300

    The day #1. Datetimes. Part 1

---
+++ 01-03-datetimes/program1.py
@@ -0,0 +1,26 @@
+from datetime import date, timedelta
+
+start_100days = date(2017, 3, 30)
+pybites_founded = date(2016, 12, 19)
+pycon_date = date(2018, 5, 8)
+
+
+def main():
+    get_hundred_days_end_date()
+    get_days_between_pb_start_first_joint_pycon()
+
+
+def get_hundred_days_end_date():
+    """Return a string of yyyy-mm-dd"""
+    plus_100 = start_100days + timedelta(days=100)
+    date_format = '%Y-%m-%d'
+    return plus_100.strftime(date_format)
+
+
+def get_days_between_pb_start_first_joint_pycon():
+    """Return the int number of days"""
+    return (pycon_date - pybites_founded).days
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01-03-datetimes/program3.py
@@ -0,0 +1,54 @@
+'''Extract datetimes from log entries and calculate the time
+   between the first and last shutdown events'''
+from datetime import datetime
+import os
+import urllib.request
+
+SHUTDOWN_EVENT = 'Shutdown initiated'
+
+logfile = os.path.join(os.path.dirname(__file__), 'tmp', 'log')
+
+urllib.request.urlretrieve('http://bit.ly/2AKSIbf', logfile)
+
+
+def main():
+    print(convert_to_datetime(loglines[6]))
+    print(convert_to_datetime(loglines[10]))
+    print(time_between_shutdowns(loglines))
+
+
+with open(logfile) as f:
+    loglines = f.readlines()
+
+
+def convert_to_datetime(line):
+    '''TODO 1:
+       Given a log line extract its timestamp and convert it to a datetime object. 
+       For example calling the function with:
+       INFO 2014-07-03T23:27:51 supybot Shutdown complete.
+       returns:
+       datetime(2014, 7, 3, 23, 27, 51)'''
+    log_date = line.split()[1]
+    date_format = '%Y-%m-%dT%H:%M:%S'
+    return datetime.strptime(log_date, date_format)
+
+
+def time_between_shutdowns(loglines):
+    '''TODO 2:
+       Extract shutdown events ("Shutdown initiated") from loglines and calculate the 
+       timedelta between the first and last one. 
+       Return this datetime.timedelta object.'''
+    shut_lines = [
+        l.strip()
+        for l in loglines
+        if SHUTDOWN_EVENT in l
+    ]
+    shut_times = [
+        convert_to_datetime(event)
+        for event in shut_lines
+    ]
+    return max(shut_times) - min(shut_times)
+
+
+if __name__ == "__main__":
+    main()
