8171597f81a91489846f5dadd95f7f1da4d1ab58
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2018-12-29 01:46:16 +0300

    Flask SQLalchemy

--- 76-78-flask/app/forms.py
+++ 76-78-flask/app/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
--- 76-78-flask/config.py
+++ 76-78-flask/config.py
@@ -1,4 +1,10 @@
 import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'any-secret-key-code'
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
